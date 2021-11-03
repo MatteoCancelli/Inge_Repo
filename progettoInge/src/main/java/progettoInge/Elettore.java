28a431599e4733005f4bf13fb03cf7be4595ed90
MatteoCancelli <matteo.cancelli.5@gmail.com>
2021-11-04 00:28:04 +0100

    creazione classi

--- progettoInge/src/main/java/progettoInge/Elettore.java
+++ progettoInge/src/main/java/progettoInge/Elettore.java
@@ -3,6 +3,19 @@ package progettoInge;
 public class Elettore {
 	private String cartaIdentita;
 
+	public Elettore(String cartaIdentita) {
+		super();
+		this.cartaIdentita = cartaIdentita;
+	}
+
+	public String getCartaIdentita() {
+		return cartaIdentita;
+	}
+
+	public void setCartaIdentita(String cartaIdentita) {
+		this.cartaIdentita = cartaIdentita;
+	}
+
 	
 	
 }
--- progettoInge/src/main/java/progettoInge/Scrutatore.java
+++ progettoInge/src/main/java/progettoInge/Scrutatore.java
@@ -3,5 +3,38 @@ package progettoInge;
 public class Scrutatore {
 	private String nome;
 	private String cognome;
-	private String cartaIdentita
+	private String cartaIdentita;
+	
+	public Scrutatore(String nome, String cognome, String cartaIdentita) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.cartaIdentita = cartaIdentita;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getCartaIdentita() {
+		return cartaIdentita;
+	}
+
+	public void setCartaIdentita(String cartaIdentita) {
+		this.cartaIdentita = cartaIdentita;
+	}
+	
+	
 }
